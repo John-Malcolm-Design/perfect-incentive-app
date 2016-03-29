a19eb1cd054132992c28a5a2c7c2ad2fcd5a7ee4
John Malcolm Anderson <hi@johnmalcolmdesign.com>
2016-03-29 14:15:18 +0100

    Created second controller API call

--- PerfectIncentiveApp/www/js/app.js
+++ PerfectIncentiveApp/www/js/app.js
@@ -1,29 +1,99 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic', 'starter.controllers'])
-
-app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
-    $urlRouterProvider.otherwise('/')
-
-    // Use x-www-form-urlencoded Content-Type
-    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+/* 
+Perfect Incentive App
+    Global 'app' module declaration. 
+    Contains global configuration and route configuration.
+    'Perfect' is the ng-app namespace as much of the code in this MVP will be 
+    used not only on the 'Perfect Incentive' app but also other products
+    including 'Perfect Centre' and 'Perfect Expense'.
     
+    MAIN PROJECT FILES:
+    App Template -  ./www/index.html 
+    Global styles - ./www/scss/ionic.app.scss
+    Controllers -   ./www/js/controllers.js
+    
+    Design References:
+    - Perfect Incentive Rebrand Board: 
+    - Websapp - Prototype
+    - Mobile App Prototype:
+*/
+
+// Declare global app module and inject dependencies. 
+var app = angular.module('perfect', ['ionic', 'perfect.controllers'])
+
+// App configuration
+app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+    // Default URL is root
+    $urlRouterProvider.otherwise('/')
+    
+    // The Perfect PaSS endpoint only accepts x-www-form-urlencoded Content-Type when posting data.
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+    $httpProvider.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+    // /**
+    //  * Converts an object to x-www-form-urlencoded serialization.
+    //  * @param {Object} obj
+    //  * @return {String}
+    //  */ 
+    // var param = function(obj) {
+    //     // Query string builder
+    //     var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+        
+    //     // Loop through object
+    //     for(name in obj) {
+    //     value = obj[name];
+            
+    //     // If contains array - parse array
+    //     if(value instanceof Array) {
+    //         for(i=0; i<value.length; ++i) {
+    //         subValue = value[i];
+    //         fullSubName = name + '[' + i + ']';
+    //         innerObj = {};
+    //         innerObj[fullSubName] = subValue;
+    //         query += param(innerObj) + '&';
+    //         }
+    //     }
+    //     // else if contains sub obj - parse object
+    //     else if(value instanceof Object) {
+    //         for(subName in value) {
+    //         subValue = value[subName];
+    //         fullSubName = name + '[' + subName + ']';
+    //         innerObj = {};
+    //         innerObj[fullSubName] = subValue;
+    //         query += param(innerObj) + '&';
+    //         }
+    //     }
+    //     // else if string - concat
+    //     else if(value !== undefined && value !== null)
+    //         query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+    //     }
+    //     // Once finished looping over object return the query string
+    //     return query.length ? query.substr(0, query.length - 1) : query;
+    // };
+
+    // // Override $http service's default transformRequest
+    // $httpProvider.defaults.transformRequest = [function(data) {
+    //     return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+    // }];
+    
+    // Route configuration
+    
+    // Introduction page
     $stateProvider.state('intro', {
         url: '/',
         templateUrl: 'views/intro.html',
         controller: 'IntroCtrl'
     })
     
+    // Main page
     $stateProvider.state('main', {
         url: '/main',
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
     })
+    
 });
 
+// App run function
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(cordova.platformId === 'ios' && window.cordova && window.cordova.plugins.Keyboard) {
--- PerfectIncentiveApp/www/js/controllers.js
+++ PerfectIncentiveApp/www/js/controllers.js
@@ -1,21 +1,25 @@
-angular.module('starter.controllers', [])
+angular.module('perfect.controllers', [])
  
 .controller('IntroCtrl', function($scope, $http, $state) {
     $scope.title = "Log In";
     $scope.user = {};
+    $scope.result = {};
+    $scope.tokenKey = 'accessToken';
+    $scope.baseUrl = 'https://perfectcard-web-test.cloudapp.net';
+    
     $scope.login = function(){
-        var self = this;
         console.log("login");
-        var user = '';
         var loginData = {
             grant_type: 'password',
+            // username: $scope.user.email,
+            // password: $scope.user.password
             username: 'hi@johnmalcolmdesign.com',
             password: '12345JmA'
         };
         delete $http.defaults.headers.common['X-Requested-With'];
         $http({
             method: 'POST',
-            url: 'https://perfectcard-web-test.cloudapp.net/Token',
+            url: $scope.baseUrl + '/Token',
             headers: {
                     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
             },
@@ -31,8 +35,7 @@ angular.module('starter.controllers', [])
                 // this callback will be called asynchronously
                 // when the response is available
                 console.log('success');
-                sessionStorage.setItem('accessToken', response.data.access_token);
-                console.log(sessionStorage.accessToken);
+                sessionStorage.setItem($scope.tokenKey, response.data.access_token);
                 $state.go('main');
             }, function errorCallback(response) {
                 // called asynchronously if an error occurs
@@ -42,6 +45,42 @@ angular.module('starter.controllers', [])
     }
 })
  
-.controller('MainCtrl', function($scope) {
- 
+.controller('MainCtrl', function($scope, $http) {
+    $scope.baseUrl = 'https://perfectcard-web-test.cloudapp.net';
+    $scope.tokenKey = 'accessToken';
+
+    //  $scope.GetTokenHeaders = function () {
+    //     var token = sessionStorage.getItem($scope.tokenKey);
+    //     var headers = {};
+    //     if (token) {
+    //         headers.Authorization = 'Bearer ' + token;
+    //     }
+    //     return headers;
+    // };
+    
+    $scope.callApi = function () {
+        var token = sessionStorage.getItem($scope.tokenKey);
+        delete $http.defaults.headers.common['X-Requested-With'];
+        $http({
+            method: 'GET',
+            url: $scope.baseUrl + '/api/GiftCardOnAccount',
+            transformRequest: function(obj) {
+                            var str = [];
+                            for(var p in obj)
+                            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                            return str.join("&");
+            },
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            }
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    };
+    
 });
